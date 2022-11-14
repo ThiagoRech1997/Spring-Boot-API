d99e49d2a63f2ae1575f62dfda1fc4a93c4c75db
Thiago Fernando Rech <thiagorech.1997@gmail.com>
2022-11-14 09:14:42 -0300

    change

--- src/main/java/br/edu/utfpr/tac/agroapi1/AgroApi1Application.java
+++ src/main/java/br/edu/utfpr/tac/agroapi1/AgroApi1Application.java
@@ -1,13 +1,23 @@
 package br.edu.utfpr.tac.agroapi1;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class AgroApi1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgroApi1Application.class, args);
 	}
 
+	@GetMapping
+	public String hellow () {
+		return "Funcinando";
+	}
 }
